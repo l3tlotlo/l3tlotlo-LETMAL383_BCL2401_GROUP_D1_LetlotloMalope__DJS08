81c640b8efdc188eb5690e482f8442992bc66813
Letlotlo Malope <Software.Engineer47@bafokeng.local>
2024-06-10 21:14:44 +0200

    enabled app rendering and home page display

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom"
-import { BrowserRouter, Routes,} from "react-router-dom"
-
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+function App() {
+  return (
+    <h1>Hello, React Router!</h1>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      
+      <Route path="/" element={<App />} />
     </Routes>
   </BrowserRouter>
 );
